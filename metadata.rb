2891036b2c98ec4e69dfd4d1e128b7cfc077884f
qubitrenagade <qubitrenegade@gmail.com>
2019-08-11 23:54:38 -0600

    Update matadata to use readme for long_description

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Qubit Renegade'
 maintainer_email 'qubitrenegade@protonmail.com'
 license 'MIT'
 description 'Installs/Configures certbot-exec'
-long_description 'Installs/Configures certbot-exec'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version '0.1.0'
 chef_version '>= 13.0'
 
