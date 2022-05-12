e8c1c2aa8a93b520449340f40ad422a16d07f181
Bram Gillemon <bram@x-plose.be>
2022-05-12 21:35:00 +0200

    added link to the conversation on slack for future reference

--- resources/certbot_repo.rb
+++ resources/certbot_repo.rb
@@ -9,7 +9,7 @@ action :create do
     Chef::Log.warn "Untested on #{platform}" if platform?('redhat', 'scientific', 'oracle')
     include_recipe 'yum-epel'
   when 'ubuntu', 'debian'
-    # Multiple people report problems with this resource, even on the slack chanel.
+    # Multiple people report problems with this resource, even on the slack chanel. https://chefcommunity.slack.com/archives/C0M1NGZE3/p1652383770807739
     # snap_package 'certbot' do
     #   package_name ['certbot']
     # end
